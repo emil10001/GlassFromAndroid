5d682275e52db7230810c15bb8ba4288ebbf5a66
John F <ejohn@feigdev.com>
2013-08-17 21:42:40 -0700

    first

---
+++ src/com/feigdev/GOauthAndroid/AuthPreferences.java
@@ -0,0 +1,40 @@
+package com.feigdev.GOauthAndroid;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by ejohn on 8/17/13.
+ */
+public class AuthPreferences {
+
+    private static final String KEY_USER = "user";
+    private static final String KEY_TOKEN = "token";
+
+    private SharedPreferences preferences;
+
+    public AuthPreferences(Context context) {
+        preferences = context
+                .getSharedPreferences("auth", Context.MODE_PRIVATE);
+    }
+
+    public void setUser(String user) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(KEY_USER, user);
+        editor.commit();
+    }
+
+    public void setToken(String password) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(KEY_TOKEN, password);
+        editor.commit();
+    }
+
+    public String getUser() {
+        return preferences.getString(KEY_USER, null);
+    }
+
+    public String getToken() {
+        return preferences.getString(KEY_TOKEN, null);
+    }
+}
